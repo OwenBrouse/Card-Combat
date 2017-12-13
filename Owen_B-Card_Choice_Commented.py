1c13df9954ac7247a33c54e5e0cdfc59657568a8
Owen Brouse <33287430+OwenBrouse@users.noreply.github.com>
2017-12-13 10:30:47 -0500

    Update Owen_B-Card_Choice_Commented.py

--- Owen_B-Card_Choice_Commented.py
+++ Owen_B-Card_Choice_Commented.py
@@ -24,6 +24,7 @@ buttonText       = ['Done'       ]
 buttonTextColour = [[0  ,0  ,0  ]]
 buttonBoxColour  = [[255,255,255]]
 
+playerName = ['player 1' , 'player 2' ]
 gameMode = 0
 
 class Button:
@@ -107,8 +108,7 @@ class Card:
         self.x += self.moveByX
         self.y += self.moveByY
         if (round(self.x) == self.destX or round(self.y) == self.destY):
-            self.computerSelect = False
-            self.flipSelect = True # just a shortcut for now
+            self.computerSelect = False# just a shortcut for now
 
     def clicked (self,mX,mY)  :
         "deteminds if the card has been clicked"
@@ -118,7 +118,7 @@ class Card:
                 
     def flip (self):
         "visualy filps the card over"
-        flipSpeed = 4
+        flipSpeed = 10
         
         if self.flipCount > 999: #unflipping
             self.width += flipSpeed
@@ -136,103 +136,125 @@ class Card:
                 self.Img = self.Id
             else:
                 self.Img = -1
-       
-temp = Card(40,65,70,111,-3) #make health/deck card
-cards.append(temp)
-cards[0].Img = cards[0].Id
 
-temp = Button(850,65,250,75,buttonText[0],buttonTextColour[0],buttonBoxColour[0]) #make button
-buttons.append(temp)
-
-for new in range (10): #ten random cards
-    temp = Card(40,60,70,111,random.randint(1,3))	
-    cards.append(temp)
-    cards[len(cards)-1].moveTo(275+(((len(cards)-2)%5)*120),300+(((len(cards)-2)//5)*180))
-
-
-while gameMode == 0 or still != len(cards)-1:
-    
-    DISPLAYSURF.blit(backPhoto, (0,-40))    #image
-    #DISPLAYSURF.fill((255,175,0))          #a solid colour
-
-    
-    for inputNumber in range(5): #draw 5 rectangles
-        pygame.draw.rect(DISPLAYSURF, (0,0,0),(80+(90*inputNumber),0,80,120))
-
-        textFont = pygame.font.Font('freesansbold.ttf', 32)
-        text = textFont.render(str(inputNumber+1), True, (255,255,255))
-        textSize = text.get_rect()
-
-        DISPLAYSURF.blit(text,((120+(90*inputNumber))-(textSize[2]/2),60-(textSize[2]/2)))
-
-    for drawButton in range(len(buttons)):
-        buttons[drawButton].display()
-   
-    still = 0
-    for drawCard in range(1,len(cards)):
-        if cards[drawCard].computerSelect == True:
-            if (round(cards[drawCard].x) != cards[drawCard].destX or round(cards[drawCard].y) != cards[drawCard].destY):
-                cards[drawCard].getMoving()
-        if cards[drawCard].flipSelect == True:
-            cards[drawCard].flip()
-        if cards[drawCard].flipSelect == False and cards[drawCard].selected == False and cards[drawCard].computerSelect == False:
-            still += 1 #counts the ammout of cards doing nothing
-            
-        cards[drawCard].display()
-    cards[0].display()
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-             pygame.quit()
-             sys.exit()
-        elif event.type == MOUSEMOTION:
-            mousex, mousey = event.pos
-            for card in range(len(cards)):
-                if cards[card].selected == True:
-                      cards[card].goTo(mousex,mousey)
-            
-        elif event.type == MOUSEBUTTONUP:
-            mouseX, mouseY = event.pos
-            for card in range(len(cards)):
-                if cards[card].selected == True: #decting if card is in a box
-                    for fakeInputNumber in range(5):
-                        if mouseY < 120 :
-                            if mouseX >(80+(90*fakeInputNumber)) and mouseX <(160+(90*fakeInputNumber))and theChosenCards[fakeInputNumber] == 0:
-                                cards[card].goTo(120+(90*fakeInputNumber),60)
-                                theChosenCards[fakeInputNumber] = cards[card].Id
-                            elif fakeInputNumber == 0: # keep unwanted cards off the bars
-                                cards[card].y = 200
-                cards[card].selected = False #unselect ALL THE CARDS 
-                            
                 
-        elif event.type == MOUSEBUTTONDOWN:
-            mouseX, mouseY = event.pos
-            for card in range(1,len(cards)):
-                if cards[card].clicked(mouseX,mouseY)==True:
-                    for fakeInputNumber in range(5): # is this card in a box if so remove it's value
-                        if cards[card].x == 120+(90*fakeInputNumber) and cards[card].y == 60:
-                            theChosenCards[fakeInputNumber] = 0
-                    cards[card].selected = True #selects dat card
-                    break
-            for button in range(len(buttons)):
-                clickedButton = buttons[button].clicked(mouseX,mouseY)
-                if clickedButton == 'Done':
-                    print(theChosenCards)
-                    gameMode = 1 #move to the next part of the game
-                    still = 0
-                    for all in range(1,len(cards)):#move the card into respective piles
-                        if cards[all].y < 65:
-                            cards[all].moveTo(120,65) 
-                        else:
-                            cards[all].moveTo(40,65) 
-                    
-##        elif event.type == KEYUP:
-##            print(gameMode,still)
-                    
-                        
-                    
+for numPlayer in range(2):
+    gameMode = 0       
+    temp = Card(40+(920*numPlayer),65,70,111,-3) #make health/deck card
+    cards.append(temp)
+    cards[0].Img = cards[0].Id
+
+    temp = Button(850-(700*numPlayer),90,250,55,buttonText[0],buttonTextColour[0],buttonBoxColour[0]) #make button
+    buttons.append(temp)
+
+    for new in range (10): #ten random cards
+        temp = Card(40+(920*numPlayer),60,70,111,random.randint(1,3))	
+        cards.append(temp)
+        cards[len(cards)-1].moveTo(275+(((len(cards)-2)%5)*120),300+(((len(cards)-2)//5)*180))
+
+
+    while gameMode == 0 or still != len(cards)-1:
+        
+        DISPLAYSURF.blit(backPhoto, (0,-40))    #image
+        #DISPLAYSURF.fill((255,175,0))          #a solid colour
+
+        
+        for inputNumber in range(5): #draw 5 rectangles
+            pygame.draw.rect(DISPLAYSURF, (0,0,0),(80+(90*inputNumber)+(400*numPlayer),0,80,120))
+
+            textFont = pygame.font.Font('freesansbold.ttf', 32)
+            text = textFont.render(str(inputNumber+1), True, (255,255,255))
+            textSize = text.get_rect()
+
+            DISPLAYSURF.blit(text,((120+(90*inputNumber)+(400*numPlayer))-(textSize[2]/2),60-(textSize[2]/2)))
+
+        for drawButton in range(len(buttons)):
+            buttons[drawButton].display()
             
-    pygame.display.update()
-    fpsClock.tick(FPS)
+            textFont = pygame.font.Font('freesansbold.ttf', 32)
+            text = textFont.render(playerName[numPlayer], True, (255,255,255))
+            textSize = text.get_rect()
+            
+            DISPLAYSURF.blit(text,(850-(700*numPlayer)-(textSize[2]/2),75-(textSize[2]/2)))
+
+       
+        still = 0
+        for drawCard in range(1,len(cards)):
+            if cards[drawCard].computerSelect == True:
+                if (round(cards[drawCard].x) != cards[drawCard].destX or round(cards[drawCard].y) != cards[drawCard].destY):
+                    cards[drawCard].getMoving()
+            if cards[drawCard].flipSelect == True:
+                cards[drawCard].flip()
+            if cards[drawCard].flipSelect == False and cards[drawCard].selected == False and cards[drawCard].computerSelect == False:
+                still += 1 #counts the ammout of cards doing nothing
+                
+            cards[drawCard].display()
+        cards[0].display()
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                 pygame.quit()
+                 sys.exit()
+            elif event.type == MOUSEMOTION:
+                mousex, mousey = event.pos
+                for card in range(len(cards)):
+                    if cards[card].selected == True:
+                          cards[card].goTo(mousex,mousey)
+                
+            elif event.type == MOUSEBUTTONUP:
+                mouseX, mouseY = event.pos
+                for card in range(len(cards)):
+                    if cards[card].selected == True: #decting if card is in a box
+                        for fakeInputNumber in range(5):
+                            if mouseY < 120 :
+                                if mouseX >(80+(90*fakeInputNumber)+(400*numPlayer)) and mouseX <(160+(90*fakeInputNumber)+(400*numPlayer))and theChosenCards[fakeInputNumber] == 0:
+                                    cards[card].goTo((120+(90*fakeInputNumber)+(400*numPlayer)),60)
+                                    theChosenCards[fakeInputNumber] = cards[card].Id
+                                elif fakeInputNumber == 0: # keep unwanted cards off the bars
+                                    cards[card].y = 200
+                    cards[card].selected = False #unselect ALL THE CARDS 
+                                
+                    
+            elif event.type == MOUSEBUTTONDOWN:
+                mouseX, mouseY = event.pos
+                for card in range(1,len(cards)):
+                    if cards[card].clicked(mouseX,mouseY)==True:
+                        for fakeInputNumber in range(5): # is this card in a box if so remove it's value
+                            if cards[card].x == (120+(90*fakeInputNumber)+(400*numPlayer)) and cards[card].y == 60:
+                                theChosenCards[fakeInputNumber] = 0
+                        cards[card].selected = True #selects dat card
+                        break
+                for button in range(len(buttons)):
+                    clickedButton = buttons[button].clicked(mouseX,mouseY)
+                    if clickedButton == 'Done':
+                        print(theChosenCards)
+                        gameMode = 1 #move to the next part of the game
+                        still = 0
+                        for alls in range(1,len(cards)):#move the card into respective piles
+                            if cards[alls].y < 65:
+                                cards[alls].moveTo(120+(760*numPlayer),65)
+                                if cards[alls].Img != -1:
+                                    cards[alls].flipSelect = True 
+                            else:
+                                cards[alls].moveTo(40+(920*numPlayer),65)
+                                if cards[alls].Img != -1:
+                                    cards[alls].flipSelect = True 
+                        
+            if event.type == KEYUP:
+                for flipAll in range(1,len(cards)):
+                    cards[flipAll].flipSelect = True
+                        
+                            
+                        
+                
+        pygame.display.update()
+        fpsClock.tick(FPS)
+
+        
+    for kill in range(len(cards)):
+        del cards[0]
+    for kill in range(len(buttons)):
+        del buttons[0]
+    theChosenCards=[0,0,0,0,0]
 pygame.quit()
 sys.exit()
